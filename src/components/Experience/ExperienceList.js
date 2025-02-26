d0ee5998b464815a68ee900f4c7367e5ab48faf0
Shreyash Dhamane <85953417+ShreyashDhamane@users.noreply.github.com>
2025-02-26 15:16:23 -0500

    Update ExperienceList.js

--- src/components/Experience/ExperienceList.js
+++ src/components/Experience/ExperienceList.js
@@ -1,8 +1,5 @@
 import studypaqFav from "/public/assets/images/studypaqFav.svg";
 import buycepsFav from "/public/assets/images/buycepsFav.png";
-import buyceps from "/public/assets/images/Buyceps.png";
-import studypaq from "/public/assets/images/studypaq.png";
-import studypaq2 from "/public/assets/images/studypaq2.png";
 import aim4u from "/public/assets/images/aim4u.png";
 import aicoms from "/public/assets/images/aicoms.png";
 import autobuddys from "/public/assets/images/autobuddys.png";
@@ -11,33 +8,35 @@ import smowcode from "/public/assets/images/smowcode.png";
 export const experiences = [
   {
     startDate: "May 2023",
-    endDate: "Present",
+    endDate: "May 2024",
     company: "Aicom",
-    role: "Software Developer and Machine Learning Intern",
+    role: "Software Developer Intern",
     type: "Internship",
     images: [aicoms],
     color: "#F5F5F5",
     workList: [
-      "Built an object detection model for retail items using YOLO and SSD and their respective pipelines from data collection to model fine-tuning.",
-      "Devised a cooking planner and sales data analysis application using Facebook’s Prophet and Flutter, respectively.",
-      "Designed and implemented an algorithm for effective staff scheduling, saving 2 hours of work weekly.",
-      "Worked on a Point of Sale (POS) system for self-food ordering using JavaScript, integrated Bluetooth Thermal Printers, Viva Card Terminals, and WebHooks for payment notifications.",
+      "Built a sales and management system using React and Flutter, saving $3,000/year and 10 hours/week",
+      "• Integrated Viva payment terminals and webhooks for processing payments exceeding 5 million euros",
+      "Implemented the entire modularized Node.js backend with CI/CD and mutex locks for data consistency",
+      "• Developed efficient custom algorithms for shift scheduling and inventory management, saving 15 hours/week",
     ],
   },
   {
     startDate: "January 2022",
     endDate: "May 2023",
     company: "Aim4U",
-    role: "Research Intern and Team Leader",
+    role: "Software Intern and Team Leader",
     type: "Research Internship",
     favIcon: buycepsFav,
     color: "#6a6a6a",
     images: [aim4u],
     workList: [
-      "Engineered skin disease detection model using Detectron2, achieved 95%+ accuracy by training the model on Nvidia GPU systems. Deployed it on AWS Lambda and integrated it with the Flutter app.",
+      "Launched a quiz platform using React (TypeScript) with a Django backend hosted on AWS EC2",
+      "Improved user experience by updating the UI across 50+ screens in Flutter",
+      "Resolved Razorpay payment issues and perfected FCM notifications, enhancing delivery time by 40%",
       "Implemented a Skin Type Indicator model with ResNet-50 and applied data engineering to achieve over 88% accuracy.",
       "Built a content-based recommendation system utilizing Vader Lexicon and skin type model to recommend suitable skin care products for effective treatment with above 90% accuracy.",
-      "Led a team of 12 junior research interns, oversaw daily updates, resolved issues, and provided regular updates to mentor. Handled machine learning, web and mobile application development.",
+      "Led a team of 12 junior interns, oversaw daily updates, resolved issues, and provided regular updates to mentor. Handled machine learning, web and mobile application development.",
     ],
   },
   {
@@ -64,8 +63,9 @@ export const experiences = [
     color: "#ecdab0",
     images: [smowcode],
     workList: [
-      "Integrated 25+ backend APIs with the website, implemented 10+ user interface screen implementations using SCSS, and released frontend updates on AWS.",
-      "Updated software packaging scripts using Inno Setup in Pascal, worked on batch scripting and CI/CD task creation on Atlassian.",
+      "Revised Node.js backend, hosted on AWS and designed 10+ responsive UI screens using SCSS",
+      "Enhanced software packaging with Inno Setup in Pascal, achieving a 15% reduction in deployment time",
+      "Directed CI/CD task creation on Atlassian platforms, reducing release cycle time by 10%",
     ],
   },
 ];
