f9c37159b0f754eabd91967cd36ed7d52108f02c
ShreyashDhamane <shreyashb.dhamane0@gmail.com>
2025-05-15 11:58:02 -0400

    update project section

--- src/components/Projects/projectsList.js
+++ src/components/Projects/projectsList.js
@@ -1,8 +1,46 @@
 import p1 from "/public/assets/images/project/p1.jpg";
 import p2 from "/public/assets/images/project/p2.jpg";
 import p3 from "/public/assets/images/project/p3.jpg";
-
+import p4 from "/public/assets/images/project/p4.jpg";
+import p5 from "/public/assets/images/project/p5.jpg";
 export const projects = [
+  {
+    title: "Nightwalkers",
+    image: p5,
+    isActive: false,
+    link: "https://github.com/ShreyashDhamane/software-engineering",
+    youtube: "https://www.youtube.com/watch?v=1FTpDpkkGlQ",
+    techs: [
+      "Next.js",
+      "Django",
+      "Firebase",
+      "PostgreSQL",
+      "Mapbox",
+      "NYC Crime Data",
+      "Python",
+      "Tailwind CSS",
+      "React",
+      "Push Notifications",
+      "WebSocket",
+    ],
+    desc: "Nightwalkers is a web platform that helps people in NYC find the safest walking routes at night. It uses real-time NYC crime data, Mapbox maps, and WebSocket-based live communication for safety alerts. Users can view crime hotspots, report incidents, get push notifications, and access helpful safety resources. The site also features a community forum and admin tools to support safer night travel across the city.",
+  },
+  {
+    title: "Nesters",
+    image: p4,
+    isActive: false,
+    link: "https://github.com/ShreyashDhamane",
+    youtube: "https://www.youtube.com/watch?v=hDoG8sCC82U",
+    techs: [
+      "Flutter",
+      "Firebase",
+      "Push Notification",
+      "Supabase",
+      "Google Maps",
+      "GitHub Actions",
+    ],
+    desc: "Nesters is a community-driven mobile app designed to simplify life for international students. Find roommates, connect with college mates, and buy or sell second-hand items easily. Explore sublease listings, discover campus-specific communities, and share student life hacks. Now available on iOS — making settling abroad easier, smarter, and more social.",
+  },
   {
     title: "ClubHive",
     image: p2,
